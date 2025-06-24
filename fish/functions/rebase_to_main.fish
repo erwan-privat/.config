e3ecae02dbd361640dfc7ccc4b1c9e5a00bc8d11
erwan-privat <erwan.privat@gmail.com>
2025-06-24 16:03:47 +0000

    rebase_to_main.fish

--- fish/functions/rebase_to_main.fish
+++ fish/functions/rebase_to_main.fish
@@ -1,8 +1,10 @@
 function rebase_to_main
+  set branch (git branch --show-current)
   git checkout main
   and git pull
-  and git rebase dev
+  and git rebase $branch
+  and git pull
   and git push
-  and git checkout dev
+  and git checkout $branch
 end
 
